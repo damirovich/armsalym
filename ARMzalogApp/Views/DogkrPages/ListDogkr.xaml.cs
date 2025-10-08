9ca6e03736d01c9ad0ad2adb9614101317253e6a
unknown <cholponay112@gmail.com>
2025-10-09 01:25:01 +0200

    updated services.

--- ARMzalogApp/Views/DogkrPages/ListDogkr.xaml.cs
+++ ARMzalogApp/Views/DogkrPages/ListDogkr.xaml.cs
@@ -1,6 +1,5 @@
-using ARMzalogApp.Models;
+using ARMzalogApp.Models.Responses;
 using Newtonsoft.Json;
-using ARMzalogApp.Views;
 using System.Windows.Input;
 using ARMzalogApp.Constants;
 using System.Collections.ObjectModel;
@@ -12,9 +11,9 @@ namespace ARMzalogApp.Views.DogkrPages;
 public partial class ListDogkr : ContentPage, INotifyPropertyChanged
 {
     public ICommand FrameTappedCommand { get; }
-    private ObservableCollection<Zavkr> _zavkrList;
+    private ObservableCollection<DogkrResponse> _zavkrList;
     private bool _isPasswordVisible = true;
-    public ObservableCollection<Zavkr> ZavkrList
+    public ObservableCollection<DogkrResponse> DogkrList
     {
         get => _zavkrList;
         set
@@ -36,12 +35,6 @@ public partial class ListDogkr : ContentPage, INotifyPropertyChanged
             _cancellationTokenSource?.Dispose();
             _cancellationTokenSource = new CancellationTokenSource();
         }
-
-        // Загружаем данные только если не загружаем сейчас
-        if (!_isLoading)
-        {
-            LoadData();
-        }
     }
 
     protected override void OnDisappearing()
@@ -55,70 +48,16 @@ public partial class ListDogkr : ContentPage, INotifyPropertyChanged
     public ListDogkr()
     {
         InitializeComponent();
-        ZavkrList = new ObservableCollection<Zavkr>();
+        DogkrList = new ObservableCollection<DogkrResponse>();
         BindingContext = this;
-    }
-
-    private async void LoadData()
-    {
-        if (_isLoading || _cancellationTokenSource == null || _cancellationTokenSource.Token.IsCancellationRequested)
-            return;
-
-        _isLoading = true;
-        try
-        {
-            activityIndicator.IsVisible = true;
-            activityIndicator.IsRunning = true;
-            activityIndicator.IsEnabled = true;
-            string departmentId = await SecureStorage.Default.GetAsync("otdel");
-            string ot_nom = "1049";
-            //string departmentId = "6";
-            using var httpClient = new HttpClient();
-            //string ot_nom = UserData.CurrentUser.UserNumber.ToString();
-            string url = ServerConstants.SERVER_ROOT_URL + "api/LoanReference/GetReferencesOfDepartment?departmentId=" + departmentId;
-            //string url = ServerConstants.SERVER_ROOT_URL + "api/LoanReference/GetReferencesOfEmployee?otNom=" + ot_nom;
-            _cancellationTokenSource.Token.ThrowIfCancellationRequested();
-            HttpResponseMessage response = await httpClient.GetAsync(url, _cancellationTokenSource.Token);
-            _cancellationTokenSource.Token.ThrowIfCancellationRequested(); // Проверяем не была ли отменена операция
-
-            if (response.IsSuccessStatusCode)
-            {
-                string responseData = await response.Content.ReadAsStringAsync();
-                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
-                ZavkrList.Clear();
-                var dataList = JsonConvert.DeserializeObject<List<Zavkr>>(responseData);
-                foreach (var item in dataList.Take(10).OrderByDescending(s => s.PositionalNumber))
-                {
-                    if (_cancellationTokenSource.Token.IsCancellationRequested)
-                        break;
-                    ZavkrList.Add(item);
-                }
-
-                OnPropertyChanged(nameof(ZavkrList));
-            }
-            else
-            {
-                Console.WriteLine($"Error: {response.StatusCode}");
-                await DisplayAlert("Ошибка", $"Ошибка сервера: {response.StatusCode}", "OK");
-            }
-            activityIndicator.IsVisible = false;
-            activityIndicator.IsRunning = false;
-            activityIndicator.IsEnabled = false;
-
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Exception: {ex.Message}");
-            await DisplayAlert("Ошибка", $"Exception: {ex.Message}", "OK");
-        }
     }
 
     private async void OnFrameTapped(object sender, EventArgs e)
     {
-        var selectedZavkr = ((sender as StackLayout)?.BindingContext as Zavkr);
+        var selectedZavkr = ((sender as StackLayout)?.BindingContext as DogkrResponse);
         if (selectedZavkr != null)
         {
-            await Navigation.PushAsync(new ZavkrPage(selectedZavkr));
+            await Shell.Current.GoToAsync("ViewKredDog");
         }
     }
 
@@ -139,30 +78,29 @@ public partial class ListDogkr : ContentPage, INotifyPropertyChanged
     {
         try
         {
-            string departmentId = await SecureStorage.Default.GetAsync("otdel");
-            using var httpClient = new HttpClient();
+            activityIndicator.IsVisible = true;
+            activityIndicator.IsRunning = true;
+            activityIndicator.IsEnabled = true;
 
-            string url = ServerConstants.SERVER_ROOT_URL + "api/LoanReference/GetOneZalog?pozn=" + pozn + "/" + departmentId;
+            using var httpClient = new HttpClient();
+            string url = ServerConstants.SERVER_ROOT_URL + "api/LoanReference/GetLoanByPN?pn=" + pozn ;
 
             HttpResponseMessage response = await httpClient.GetAsync(url);
 
             if (response.IsSuccessStatusCode)
             {
                 string responseData = await response.Content.ReadAsStringAsync();
-                ZavkrList.Clear();
-
-                var dataList = JsonConvert.DeserializeObject<List<Zavkr>>(responseData);
+                var dataList = JsonConvert.DeserializeObject<List<DogkrResponse>>(responseData);
 
                 foreach (var item in dataList)
                 {
-                    ZavkrList.Add(item);
-
+                    DogkrList.Add(item);
                 }
                 if (dataList.Count > 0)
                 {
-                    DisplayAlert("Успешно", "Заявка найдена ", "OK");
+                    DisplayAlert("Успешно", "Кредитный договор не найден", "OK");
                 }
-                OnPropertyChanged(nameof(ZavkrList));
+                OnPropertyChanged(nameof(DogkrList));
 
             }
             else
@@ -174,37 +112,16 @@ public partial class ListDogkr : ContentPage, INotifyPropertyChanged
         {
             Console.WriteLine($"Exception: {ex.Message}");
         }
+        activityIndicator.IsVisible = false;
+        activityIndicator.IsRunning = false;
+        activityIndicator.IsEnabled = false;
     }
     private void ClearFilter(object sender, EventArgs e)
     {
-        LoadData();
         numberEntry.Text = string.Empty;
-        ZavkrList.Clear();
+        DogkrList.Clear();
     }
-    private void HideProperty(object sender, EventArgs e)
-    {
-        if (ZavkrList.Count > 0)
-        {
-            bool hide = ZavkrList.All(z => !z.IsSecretHidden);
-            foreach (var zavkr in ZavkrList)
-            {
-                zavkr.IsSecretHidden = hide;
-            }
-            _isPasswordVisible = !_isPasswordVisible;
-            UpdatePasswordVisibility();
-        }
-    }
-    private void UpdatePasswordVisibility()
-    {
-        if (_isPasswordVisible)
-        {
-            showPasswordIcon.Source = "hide_icon.png";
-        }
-        else
-        {
-            showPasswordIcon.Source = "show_icon.png";
-        }
-    }
+    
 
     #region INotifyPropertyChanged
     public event PropertyChangedEventHandler PropertyChanged;
--- ARMzalogApp/Views/ZavkrPages/CreateLoanZarp.xaml.cs
+++ ARMzalogApp/Views/ZavkrPages/CreateLoanZarp.xaml.cs
@@ -11,12 +11,21 @@ public partial class CreateLoanZarp : ContentPage
 	{
 		InitializeComponent();
         CurrencyList = new ObservableCollection<CurrencyResponse>();
+        LoanTypeList = new ObservableCollection<LoanTypeResponse>();
+        SubProductList = new ObservableCollection<Spr>();
+        PurposesList = new ObservableCollection<Spr>();
 
-        GetDataFromServer();
+        GetDataCurrencies();
+        GetDataLoanType();
+        GetDataSubProduct();
+        GetDataPurpose();
         BindingContext = this;
     }
 
     public ObservableCollection<CurrencyResponse> CurrencyList { get; set; }
+    public ObservableCollection<LoanTypeResponse> LoanTypeList { get; set; }
+    public ObservableCollection<Spr> SubProductList { get; set; }
+    public ObservableCollection<Spr> PurposesList { get; set; }
 
     private CurrencyResponse _selectedCurrency;
     public CurrencyResponse SelectedCurrency
@@ -25,34 +34,114 @@ public partial class CreateLoanZarp : ContentPage
         set
         {
             _selectedCurrency = value;
-            OnPropertyChanged(); // если у вас INotifyPropertyChanged
+            OnPropertyChanged(); // если INotifyPropertyChanged
         }
     }
+
+    private CurrencyResponse _selectedLoanType;
+    public CurrencyResponse SelectedLoanType
+    {
+        get => _selectedLoanType;
+        set
+        {
+            _selectedLoanType = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private CurrencyResponse _selectedSubProduct;
+    public CurrencyResponse SelectedSubProduct
+    {
+        get => _selectedSubProduct;
+        set
+        {
+            _selectedSubProduct = value;
+            OnPropertyChanged();
+        }
+    }
+    private CurrencyResponse _selectedPurpose;
+    public CurrencyResponse SelectedPurpose
+    {
+        get => _selectedPurpose;
+        set
+        {
+            _selectedPurpose = value;
+            OnPropertyChanged();
+        }
+    }
+
 
     private async void OnOpiuPage(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("OpiuPage");
     }
 
-    private async void GetDataFromServer()
+    private async void GetDataCurrencies()
     {
-        
         using var httpClient = new HttpClient();
-
         string url = ServerConstants.SERVER_ROOT_URL + "api/LoanReference/GetCurrencies";
-
         HttpResponseMessage response = await httpClient.GetAsync(url);
 
         if (response.IsSuccessStatusCode)
         {
             string responseData = await response.Content.ReadAsStringAsync();
-
             List<CurrencyResponse> dataList = JsonConvert.DeserializeObject<List<CurrencyResponse>>(responseData);
             foreach (var item in dataList)
             {
                 CurrencyList.Add(item);
             }
+        }
+    }
 
+    private async void GetDataLoanType()
+    {
+        using var httpClient = new HttpClient();
+        string url = ServerConstants.SERVER_ROOT_URL + "api/LoanReference/GetLoanTypes";
+        HttpResponseMessage response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseData = await response.Content.ReadAsStringAsync();
+            List<LoanTypeResponse> dataList = JsonConvert.DeserializeObject<List<LoanTypeResponse>>(responseData);
+            foreach (var item in dataList)
+            {
+                LoanTypeList.Add(item);
+            }
+
+        }
+    }
+
+    private async void GetDataSubProduct()
+    {
+        using var httpClient = new HttpClient();
+        string url = ServerConstants.SERVER_ROOT_URL + "api/LoanReference/GetLoanSubProducts";
+        HttpResponseMessage response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseData = await response.Content.ReadAsStringAsync();
+            List<Spr> dataList = JsonConvert.DeserializeObject<List<Spr>>(responseData);
+            foreach (var item in dataList)
+            {
+                SubProductList.Add(item);
+            }
+        }
+    }
+
+    private async void GetDataPurpose()
+    {
+        using var httpClient = new HttpClient();
+        string url = ServerConstants.SERVER_ROOT_URL + "api/LoanReference/GetLoanPurposes";
+        HttpResponseMessage response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseData = await response.Content.ReadAsStringAsync();
+            List<Spr> dataList = JsonConvert.DeserializeObject<List<Spr>>(responseData);
+            foreach (var item in dataList)
+            {
+                PurposesList.Add(item);
+            }
         }
     }
 
